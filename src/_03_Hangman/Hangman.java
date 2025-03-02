ff3e1f7358fb24189763ab966f8744eaaf9d6dbc
league <league@wts14-3.attlocal.net>
2025-03-02 11:26:51 -0800

    :(

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,5 +1,30 @@
 package _03_Hangman;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class Hangman {
 
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	
+	
+	void setup(){
+		frame.add(panel);
+		frame.setVisible(true);
+		panel.add(label);
+		frame.setTitle("Hang Man");
+		frame.pack();
+	}
+	
+	 public static void main(String[] args) {
+		 Hangman man = new Hangman();
+		man.setup();
+	String num = JOptionPane.showInputDialog(null, "please enter a number between 0 and 266" );
+	Utilities.readRandomLineFromFile(num);
+	System.out.println(Utilities.readRandomLineFromFile("dictionary.txt"));
+	}
 }
