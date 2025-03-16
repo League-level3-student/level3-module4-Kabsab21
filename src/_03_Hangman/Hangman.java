50245e4e42f3e24f65bf9cf4d86c55489e88e9b9
league <league@wts14-3.attlocal.net>
2025-03-16 11:28:19 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,16 +1,27 @@
 package _03_Hangman;
 
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.stream.IntStream;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Hangman {
-
+	static ArrayDeque<String> q = new ArrayDeque<String>();
+	static ArrayList<String> list = new ArrayList<String>();
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	static String word;
+	static int lives = 10;
+	int score = 0;
 	
+	 
 	
 	void setup(){
 		frame.add(panel);
@@ -22,9 +33,56 @@ public class Hangman {
 	
 	 public static void main(String[] args) {
 		 Hangman man = new Hangman();
-		man.setup();
-	String num = JOptionPane.showInputDialog(null, "please enter a number between 0 and 266" );
-	Utilities.readRandomLineFromFile(num);
-	System.out.println(Utilities.readRandomLineFromFile("dictionary.txt"));
-	}
+		 man.listmaker();
+		 man.setup();
+		 man.game();
+		
+		}
+		
+		
+		void listmaker() {
+			
+			String num = JOptionPane.showInputDialog(null, "please enter a number between 0 and 266" );
+			int nuum = Integer.parseInt(num);
+			while( nuum > list.size()) {
+				Boolean equal = false;
+				String wrd = Utilities.readRandomLineFromFile("dictionary.txt");
+				for( int i = 0; i < list.size(); i++) {
+					
+					
+					if( wrd.equals( list.get(i))) {
+						equal = true;
+						break;
+					}
+				}
+				
+				if( equal == false ) {
+						list.add(wrd);
+						System.out.println(list.size()+" word added: "+wrd);
+				}
+				
+			}
+			
+			for( int i = 0; i < list.size(); i++) {
+				q.add(list.get(i));
+			}
+			System.out.println(q);
+			
+			
+			
+		}
+		
+		void game() {
+			word = q.pop();
+			
+		}
+		
+		
+	// auhhh add thing for typing letters, make word into an array, connected to another array that is just blank,
+	// when user guess a letter, iterate through array to find letters macht, if letters match replace __ in blank array with letter
+	// if not, reduce the lives by one. onece arrays are fully equal to each other, write score one if win, if run out of lives lose.
+	// give user their score out of all games played.
+	
 }
+
+
